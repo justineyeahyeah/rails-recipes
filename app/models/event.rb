72c5ebd6ff85d2753734d688d30dde840f8d0750
justineyeahyeah <justineyeah@163.com>
2017-06-20 10:38:17 +0800

    add radio button

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,16 +4,22 @@ class Event < ApplicationRecord
 
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, :with => /\A[a-z0-9\-]\z/
+  belongs_to :category, :optional => true
 
+  before_validation :generate_friendly_id, :on => :create
 
- def to_param
-   self.friendly_id
- end
+   def to_param
+     self.friendly_id
+   end
 
- protected
+   STATUS = ["draft", "public", "private"]
+   validates_inclusion_of :status, :in => STATUS
 
- def generate_friendly_id
-   self.friendly_id ||= SecureRandom.uuid
- end
+   protected
+
+   def generate_friendly_id
+     self.friendly_id ||= SecureRandom.uuid
+   end
+
 
 end
